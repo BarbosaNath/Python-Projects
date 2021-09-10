5a4105bd5ad88c0225cbe6fd3125c9743cc90d92
Nath <Playerxmatt@gmail.com>
2021-09-10 20:02:32 -0300

    made add and delete into functions

--- Outros/Learning Wallpaper/LWall.py
+++ Outros/Learning Wallpaper/LWall.py
@@ -10,6 +10,20 @@ from args import args
 
 SPI_SETDESKWALLPAPER = 20
 
+def add_to_csv(words, path):
+    for word in words:
+        word = word.split(',')
+        with open(path, 'a+', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(word)
+
+def delete_from_csv(words, path):
+    for word_del in words:
+        csv_file = pd.read_csv(path)
+        csv_file.get_index('word', inplace(True))
+        csv_file.drop(word_del)
+        csv_file.to_csv(path)
+
 
 # Open json file containing the configuration
 with open('config.json', 'r') as json_file:
@@ -20,29 +34,22 @@ editor = ImageEditor(config_dict)
 
 
 # Add Words
-if args.add != []:
-    for word in args.add:
-        word = word.split(',')
-        with open(editor.file_path, 'a+', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow(word)
-
+if args.add != []: add_to_csv(args.add, editor.file_path)
 # Remove words
-if args.delete != []:
-    for word_del in args.delete:
-        csv_file = pd.read_csv(editor.file_path)
-        csv_file.get_index('word', inplace(True))
-        csv_file.drop(word_del)
-        csv_file.to_csv(editor.file_path)
+if args.delete != []: delete_from_csv(args.delete, editor.file_path)
 
-# recreated the editor so any new config is updated
+
+# recreated the editor so any new config or words are updated
 editor = ImageEditor(config_dict)
 
+
+# open the image on your default image viewer
 if args.show:
     editor.draw().show()
 # Save Images
 if args.save is not None:
     editor.draw().save(args.save)
+# set the image made as your new wallpaper
 if args.set:
     editor.draw().save(os.getcwd() + r'\image.png')
     ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, os.getcwd() + r'\image.png', 0)
